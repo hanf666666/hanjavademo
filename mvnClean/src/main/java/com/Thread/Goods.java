d3e89cdb5e8831bcc6cb6f9f53958c8f830974ea
hanjing <hanjing@meitianiot.com>
2022-01-13 22:27:31 +0800

    打印和pdf合并

---
+++ mvnClean/src/main/java/com/Thread/Goods.java
@@ -0,0 +1,21 @@
+package com.Thread;
+
+public class Goods {
+    private String name;
+    private Double price;
+    private Integer number;
+    public Goods(String name,Double price,Integer number){
+        this.name = name;
+        this.price = price;
+        this.number = number;
+    }
+    public Double getTotalPrice(){
+        return this.price*this.number;
+    }
+
+    public String toString() {
+        return this.name+"\t"+"￥"+this.price+"\t"
+                +this.number+"\t"+this.getTotalPrice();
+    }
+
+}
