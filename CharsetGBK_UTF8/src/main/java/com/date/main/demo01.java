ddb8a9e4a9acd81e922d00a3b7b7423258ebf129
hanjing <hanjing@meitianiot.com>
2022-01-14 16:24:12 +0800

    添加时间模块

--- CharsetGBK_UTF8/src/main/java/com/date/main/demo01.java
+++ CharsetGBK_UTF8/src/main/java/com/date/main/demo01.java
@@ -7,16 +7,17 @@ import java.io.File;
 
 /**
  * to do
- *
+ * 所有项目统一用utf-8
+ * 一定注意编码问题,这个都没用用
  * @author Hj
  * @date 2021/12/16
  */
 public class demo01 {
     public static void main(String[] args) {
-        final String file1 = "D:\\ideaspace\\javaspace\\zijidemo\\hanjavademo\\CharsetGBK_UTF8\\src\\main\\java\\com\\it\\data";
+        final String file1 = "D:\\ideaspace\\javaspace\\zijidemo\\hanjavademo2";
         try {
-//            TraversalUtil.listDirectory(new File(file1), ConvertEncodetype.TOUTF);
-            TraversalUtil.listDirectory(new File(file1), ConvertEncodetype.TOGBK);
+            TraversalUtil.listDirectory(new File(file1), ConvertEncodetype.TOUTF);
+//            TraversalUtil.listDirectory(new File(file1), ConvertEncodetype.TOGBK);
 
 
         } catch (Exception e) {
