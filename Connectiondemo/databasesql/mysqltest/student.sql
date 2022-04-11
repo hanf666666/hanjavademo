a8a404c8e59b7efb842e1a8b2f8d2e375faaf3a5
hanf666666 <hanjing_xl@163.com>
2022-04-11 14:11:45 +0800

    mysql的测试库

--- Connectiondemo/databasesql/mysqltest/student.sql
+++ Connectiondemo/databasesql/mysqltest/student.sql
@@ -1,39 +1,28 @@
-/*
-SQLyog Ultimate v8.32 
-MySQL - 5.7.19-log : Database - replicastest
-*********************************************************************
-*/
 
+DROP DATABASE  IF EXISTS test20210707;
+CREATE DATABASE test20210707;
+USE test20210707;
+CREATE TABLE student(id INT,NAME VARCHAR(20),name2 VARCHAR(20),age INT,age2 INT,created_dt DATETIME,created_dt2 DATETIME(3));
 
-/*!40101 SET NAMES utf8 */;
-
-/*!40101 SET SQL_MODE=''*/;
-
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
-/*Table structure for table `student` */
-
-CREATE TABLE `student` (
-  `id` int(11) NOT NULL,
-  `NAME` varchar(20) DEFAULT NULL,
-  `name2` varchar(20) DEFAULT NULL,
-  `age` int(11) DEFAULT NULL,
-  `age2` int(11) DEFAULT NULL,
-  `created_dt` datetime DEFAULT NULL,
-  `created_dt2` datetime(3) DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
-/*Data for the table `student` */
-
-INSERT INTO student(id,NAME,name2,age,age2,created_dt,created_dt2) VALUES(1,'demo','demo01',21,21,'2021-01-01 12:11:11','2021-01-01 12:11:11.111');
-INSERT INTO student(id,NAME,name2,age,age2,created_dt,created_dt2) VALUES(2,'demo','demo01',21,21,'2021-01-01 12:11:11','2021-01-01 12:11:11.112');
-INSERT INTO student(id,NAME,name2,age,age2,created_dt,created_dt2) VALUES(3,'demo','demo01',21,21,'2021-01-01 12:11:11','2021-01-01 12:11:11.113');
-INSERT INTO student(id,NAME,name2,age,age2,created_dt,created_dt2) VALUES(4,'demo','demo01',21,21,'2021-01-01 12:11:11','2021-01-01 12:11:11.114');
-INSERT INTO student(id,NAME,name2,age,age2,created_dt,created_dt2) VALUES(5,'demo','demo01',21,21,'2021-01-01 12:11:11','2021-01-01 12:11:11.115');
-INSERT INTO student(id,NAME,name2,age,age2,created_dt,created_dt2) VALUES(6,'demo','demo01',21,21,'2021-01-01 12:11:11','2021-01-01 12:11:11.116');
-INSERT INTO student(id,NAME,name2,age,age2,created_dt,created_dt2) VALUES(7,'demo','demo01',21,21,'2021-01-01 12:11:11','2021-01-01 12:11:11.117');
+INSERT INTO student(id,NAME,name2,age,age2,created_dt,created_dt2) VALUES(1,'º«','º«¾²',21,21,'2021-01-01 12:11:11','2021-01-01 12:11:11.111');
+INSERT INTO student(id,NAME,name2,age,age2,created_dt,created_dt2) VALUES(2,'º«','º«¾²',21,21,'2021-01-01 12:11:11','2021-01-01 12:11:11.112');
+INSERT INTO student(id,NAME,name2,age,age2,created_dt,created_dt2) VALUES(3,'º«','º«¾²',21,21,'2021-01-01 12:11:11','2021-01-01 12:11:11.113');
+INSERT INTO student(id,NAME,name2,age,age2,created_dt,created_dt2) VALUES(4,'º«','º«¾²',21,21,'2021-01-01 12:11:11','2021-01-01 12:11:11.114');
+INSERT INTO student(id,NAME,name2,age,age2,created_dt,created_dt2) VALUES(5,'º«','º«¾²',21,21,'2021-01-01 12:11:11','2021-01-01 12:11:11.115');
+INSERT INTO student(id,NAME,name2,age,age2,created_dt,created_dt2) VALUES(6,'º«','º«¾²',21,21,'2021-01-01 12:11:11','2021-01-01 12:11:11.116');
+INSERT INTO student(id,NAME,name2,age,age2,created_dt,created_dt2) VALUES(7,'º«','º«¾²',21,21,'2021-01-01 12:11:11','2021-01-01 12:11:11.117');
 INSERT INTO student(id,NAME,name2,age,age2,created_dt,created_dt2) VALUES
-(8,'demo','demo01',21,21,'2021-01-01 12:11:11','2021-01-01 12:11:11.118'),
-(9,'demo','demo01',21,21,'2021-01-01 12:11:11','2021-01-01 12:11:11.119')
+(8,'º«','º«¾²',21,21,'2021-01-01 12:11:11','2021-01-01 12:11:11.118'),
+(9,'º«','º«¾²',21,21,'2021-01-01 12:11:11','2021-01-01 12:11:11.119'),
+(10,'º«','º«¾²',21,21,'2021-01-01 12:11:11','2021-01-01 12:11:11.120');
+
+SELECT * FROM student;
+
+
+UPDATE student SET age=11 WHERE id=1;
+UPDATE student SET age=26 WHERE id>1;
+DELETE FROM student WHERE  id=1;
+DELETE FROM student WHERE  id>5;
+
+
+TRUNCATE TABLE student;
