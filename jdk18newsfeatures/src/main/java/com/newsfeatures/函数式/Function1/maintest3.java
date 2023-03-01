3a2e4d63a2db627ce134352782bb9d0a4096c393
hanf666666 <hanjing_xl@163.com>
2023-03-01 14:42:38 +0800

    jdk Function原始写法写

---
+++ jdk18newsfeatures/src/main/java/com/newsfeatures/函数式/Function1/maintest3.java
@@ -0,0 +1,37 @@
+package com.newsfeatures.函数式.Function1;
+
+import java.util.function.Function;
+
+/**
+ * to do
+ *
+ * @author Hj
+ * @date 2021/12/2
+ */
+public class maintest3 {
+    public static void main(String[] args) {
+
+        //简写
+//        Function<Integer, Integer> toIntegerFunction = (a)->{
+//
+//            System.out.println(a+"=================>");
+//            return a;
+//        };
+
+        //   jdk Function原始写法写
+        Function<Integer, Integer> toIntegerFunction = new Function<Integer, Integer>() {
+            @Override
+            public Integer apply(Integer a) {
+                System.out.println(a + "=================>");
+
+                return a;
+            }
+        };
+
+
+        Integer apply = toIntegerFunction.apply(2);
+        System.out.println("@@@@@@@" + apply);
+
+
+    }
+}
