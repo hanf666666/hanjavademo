f5cb1545ccb5ca2ee088ad044825c42b61d44cbc
hanf666666 <hanjing_xl@163.com>
2023-07-12 18:01:47 +0800

    新特性

--- jdk1dot8newsfeatures/src/main/java/com/newsfeatures/函数式/Function1/maintest3.java
+++ jdk1dot8newsfeatures/src/main/java/com/newsfeatures/函数式/Function1/maintest3.java
@@ -11,7 +11,7 @@ import java.util.function.Function;
 public class maintest3 {
     public static void main(String[] args) {
 
-        //简写   (a)->{}就是new了个引用对象
+        //简写   (a)->{}就是new了个引用对象 及写了个实现类
 //        Function<Integer, Integer> toIntegerFunction = (a)->{
 //
 //            System.out.println(a+"=================>");
