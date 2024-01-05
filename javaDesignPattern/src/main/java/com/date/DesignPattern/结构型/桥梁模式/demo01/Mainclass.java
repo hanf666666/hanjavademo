511835fa5e710e489b9cc21d0a62125d17db2eb9
hanf666666 <hanjing_xl@163.com>
2024-01-05 11:16:23 +0800

    桥接

--- javaDesignPattern/src/main/java/com/date/DesignPattern/结构型/桥梁模式/demo01/Mainclass.java
+++ javaDesignPattern/src/main/java/com/date/DesignPattern/结构型/桥梁模式/demo01/Mainclass.java
@@ -1,7 +1,10 @@
 package com.date.DesignPattern.结构型.桥梁模式.demo01;
 
 
-
+/**
+ * 其实就是理解代码抽象和解耦
+ * 笔和形状有关联 需解耦更加形象,灵活
+ */
 public class Mainclass {
     public static void main(String[] args) {
         Shape greenCircle = new Circle(10, new GreenPen());
