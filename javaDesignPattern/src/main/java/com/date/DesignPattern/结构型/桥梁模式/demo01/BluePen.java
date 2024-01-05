687fe295d8703cf1f9d47438313a14c8574277d1
hanf666666 <hanjing_xl@163.com>
2024-01-05 11:12:51 +0800

    适配器和构造

--- javaDesignPattern/src/main/java/com/date/DesignPattern/结构型/桥梁模式/demo01/BluePen.java
+++ javaDesignPattern/src/main/java/com/date/DesignPattern/结构型/桥梁模式/demo01/BluePen.java
@@ -3,6 +3,5 @@ package com.date.DesignPattern.结构型.桥梁模式.demo01;
 public class BluePen implements DrawAPI {
     @Override
     public void draw(int radius, int x, int y) {
-        System.out.println("??BluePen?????radius:" + radius + ", x:" + x + ", y:" + y);
-    }
+        System.out.println("用蓝色笔画图，radius:" + radius + ", x:" + x + ", y:" + y);    }
 }
--- javaDesignPattern/src/main/java/com/date/DesignPattern/结构型/桥梁模式/demo01/GreenPen.java
+++ javaDesignPattern/src/main/java/com/date/DesignPattern/结构型/桥梁模式/demo01/GreenPen.java
@@ -3,6 +3,5 @@ package com.date.DesignPattern.结构型.桥梁模式.demo01;
 public class GreenPen implements DrawAPI {
     @Override
     public void draw(int radius, int x, int y) {
-        System.out.println("???????????radius:" + radius + ", x:" + x + ", y:" + y);
-    }
+        System.out.println("用绿色笔画图，radius:" + radius + ", x:" + x + ", y:" + y);    }
 }
