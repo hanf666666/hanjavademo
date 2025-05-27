4d358f7b1bb1f8bb70f60da517f3093c589e62ac
hanf666666 <hanjing_xl@163.com>
2025-05-27 08:53:01 +0800

    dsad

--- mvnClean/src/main/java/com/scandemo/Migrate/MigratePreDemo.java
+++ mvnClean/src/main/java/com/scandemo/Migrate/MigratePreDemo.java
@@ -17,12 +17,14 @@ import java.util.List;
 public class MigratePreDemo {
     public static void main(String[] args) {
 
-        String srcFileName="application-k8spre.properties";
-        String dedfileName="application-txpre.properties";
+        String srcFileName="application-txpre.properties";
+        String dedfileName="application-sntxtest.properties";
         //递归遍历目录以及子目录中的所有文件 可以加过滤条件
 //        List<File> files = FileUtil.loopFiles("D:\\ideaspace\\javaspace\\meitikeji\\", new FileFilter() {
 //        List<File> files = FileUtil.loopFiles("D:\\ideaspace\\javaspace\\meitikeji\\java-bj\\ClearingPlat\\", new FileFilter() {
-        List<File> files = FileUtil.loopFiles("D:\\ideaspace\\javaspace\\meitikeji\\java\\yun\\meisoo-appmanage-activity-service\\", new FileFilter() {
+//        List<File> files = FileUtil.loopFiles("D:\\ideaspace\\javaspace\\meitikeji\\java-bj\\BossManage\\meisoo-bossmanage-service\\", new FileFilter() {
+        List<File> files = FileUtil.loopFiles("D:\\ideaspace\\javaspace\\meitikeji\\java\\yun\\", new FileFilter() {
+//        List<File> files = FileUtil.loopFiles("D:\\ideaspace\\javaspace\\meitikeji\\java\\common_service\\", new FileFilter() {
 //        List<File> files = FileUtil.loopFiles("D:\\ideaspace\\javaspace\\meitikeji\\acquisition-platform\\", new FileFilter() {
 //        List<File> files = FileUtil.loopFiles("D:\\ideaspace\\javaspace\\meitikeji\\java-bj\\BossManage\\meisoo-bossmanage-service\\", new FileFilter() {
             @Override
@@ -55,26 +57,27 @@ public class MigratePreDemo {
                     String line;
                     while ((line = reader.readLine()) != null) {
                         // 修改每一行的内容，这里只是一个简单的示例，你可以根据需要进行修改
-                        String modifiedLine = line.replace("fasdfasdfasdfasd", "fasdfasdfasdfasd");
+                        String modifiedLine = line.replace("http://eureka-0.eureka.pre.svc.cluster.local:8761/center/eureka/,http://eureka-1.eureka.pre.svc.cluster.local:8761/center/eureka/,http://eureka-2.eureka.pre.svc.cluster.local:8761/center/eureka/", "http://eureka-0.eureka.test.svc.cluster.local:8761/center/eureka/,http://eureka-1.eureka.test.svc.cluster.local:8761/center/eureka/,http://eureka-2.eureka.test.svc.cluster.local:8761/center/eureka/");
 //                         modifiedLine = line.replace("mysql-service.dev.svc.cluster.local", "10.50.254.22");
                         // mysql
-                         modifiedLine = modifiedLine.replace("rm-2zeo71tq83aq0381u.mysql.rds.aliyuncs.com", "10.50.254.15:3306");
+                         modifiedLine = modifiedLine.replace("10.50.254.15:3306", "10.50.254.22:13309");
                          //mongo
 //                        modifiedLine = modifiedLine.replace("spring.data.mongodb.uri=mongodb://mongo-svc.test1.svc.cluster.local:27017/log_db", "spring.data.mongodb.uri=mongodb://db_log:Cb6JFb5HNt@@U2A*@mongo-svc.test.svc.cluster.local:27018/db_log");
-                         modifiedLine = modifiedLine.replace("mongodb://db_log:Jj89757***@dds-2ze2242abf52143433270.mongodb.rds.aliyuncs.com:3717/db_log", "mongodb://db_log:l3W!Fh!8TNwQ@10.50.254.110:27017,10.50.254.39:27017,10.50.254.143:27017/db_log?authSource=admin&replicaSet=cmgo-pksm3i0f_0");
-                         modifiedLine = modifiedLine.replace("mongodb://clearing_plat_user:CwQ5yCWpLP@dds-2ze2242abf52143433270.mongodb.rds.aliyuncs.com:3717/clearing_plat", "mongodb://clearing_plat_user:CwQ5yCWpLP@10.50.254.110:27017,10.50.254.39:27017,10.50.254.143:27017/clearing_plat?replicaSet=cmgo-pksm3i0f_0&authSource=admin");
+                         modifiedLine = modifiedLine.replace("mongodb://db_log:l3W!Fh!8TNwQ@10.50.254.110:27017,10.50.254.39:27017,10.50.254.143:27017/db_log?authSource=admin&replicaSet=cmgo-pksm3i0f_0", "mongodb://root:123123@10.50.254.22:27018/db_log?authSource=admin");
                          //redis
-                         modifiedLine = modifiedLine.replace("r-2zef7cf6dadbdf74.redis.rds.aliyuncs.com", "10.50.254.8");
-                         modifiedLine = modifiedLine.replace("spring.redis.password=Dy&hk78Qaz$98", "spring.redis.password=DyhkQazBs989");
-                         modifiedLine = modifiedLine.replace("redisson.password=Dy&hk78Qaz$98", "redisson.password=DyhkQazBs989");
+                         modifiedLine = modifiedLine.replace("10.50.254.8", "10.50.254.22");
+                         modifiedLine = modifiedLine.replace("spring.redis.password=DyhkQazBs989", "spring.redis.password=123");
+                         modifiedLine = modifiedLine.replace("spring.redis.port=6379", "spring.redis.port=16379");
+                         modifiedLine = modifiedLine.replace("redisson.password=DyhkQazBs989", "redisson.password=123");
                          //kafka
-                         modifiedLine = modifiedLine.replace("172.17.134.26:9092,172.17.134.27:9092,172.17.134.28:9092", "10.50.254.4:9092");
+                         modifiedLine = modifiedLine.replace("api_deploy", "root");
+                         modifiedLine = modifiedLine.replace("1qaz@WSX", "password");
 
 
-                        //file.store.default.upload.type TencentCOS
-                        if(line.contains("file.store.default.upload.type")){
-                            modifiedLine ="file.store.default.upload.type=TencentCOS";
-                        }
+//                        //file.store.default.upload.type TencentCOS
+//                        if(line.contains("file.store.default.upload.type")){
+//                            modifiedLine ="file.store.default.upload.type=TencentCOS";
+//                        }
 
 
 //                        modifiedLine = modifiedLine.replace("spring.datasource.username=meisooWr", "spring.datasource.username=root");
