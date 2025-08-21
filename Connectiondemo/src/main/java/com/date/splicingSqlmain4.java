85de306e44929121c803d4dcc6d2eb1c3e48f3de
hanf666666 <hanjing_xl@163.com>
2025-08-21 15:49:03 +0800

    suinin

--- Connectiondemo/src/main/java/com/date/splicingSqlmain4.java
+++ Connectiondemo/src/main/java/com/date/splicingSqlmain4.java
@@ -16,6 +16,18 @@ public class splicingSqlmain4 {
                 "NULL AS  refundStatus,NULL AS disputeBackPay,0  As rechargeElectricityDegrees \n" +
                 "from park_card_order pco where pco.deleted =0 and pco.phone =:phone AND pco.created_dt > :createDate and pco.order_status=2 AND pco.invoice_id IS NULL";
 
+
+        sql = "select a.* from pay_route_info a \n" +
+                "join pay_route_business b on a.id=b.pay_route_id \n" +
+                "where a.deleted =0 and a.pay_app_id =?1 \n" +
+                "and b.deleted =0 and b.pay_business_id in ?2";
+
+        sql = "select a.* from pay_route_info a \n" +
+                "join pay_route_business b on a.id=b.pay_route_id \n" +
+                "join pay_route_owner c on a.id =c.pay_route_id \n" +
+                "where a.deleted =0 and a.pay_app_id =?1 \n" +
+                "and b.deleted =0 and b.pay_business_id in ?2\n" +
+                "and c.deleted =0 and c.park_owner_id =?3";
         System.out.println(sql);
 
 
