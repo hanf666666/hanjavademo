4c111e3771d39d4fa9be1a2231a546603caea988
hanf666666 <hanjing_xl@163.com>
2025-09-08 20:48:25 +0800

    生成pdf缺失中文输出

--- Connectiondemo/src/main/java/com/date/splicingSqlmain3.java
+++ Connectiondemo/src/main/java/com/date/splicingSqlmain3.java
@@ -13,7 +13,7 @@ import java.util.concurrent.atomic.AtomicReference;
  */
 public class splicingSqlmain3 {
     public static void main(String[] args) {
-        String fieldsSql = "'”ÂBXE897','”ÂA03Z07','”ÂAPE841','”ÂB608NU','”ÂB612L8','”ÂAAZ6780','”ÂABA0111','”ÂA735LC','”ÂB268PX','”ÂAH62P3','”ÂAA029L','”ÂBKZ778','”ÂBYQ580','”ÂAS6774','”ÂAH01P3'";
+        String fieldsSql = "'”ÂAK9C03','”ÂAAT2393','”ÂA81TT0','”ÂBKC070','”ÂBL0842','”ÂA67P7P','”ÂBD768B','”ÂABU8150','”ÂDFC937','”ÂA892SD','”ÂABW8716','”ÂAGE3268','”ÂAS1B29','”ÂAM59K2','”ÂABN5396','”ÂD06902','”ÂADA7298','”ÂA352RN','”ÂB563GW','”ÂAOK849','”ÂDW6666','”ÂBTV824','”ÂA680QQ','”ÂABV9717'";
         AtomicReference<String> rowStr= new AtomicReference<>("");
         Arrays.stream(fieldsSql.split(",")).forEach(row->{
 //            row = row.substring(1, row.length()-1);
@@ -21,16 +21,16 @@ public class splicingSqlmain3 {
                 rowStr.set(rowStr.get()+","+row);
 //                System.out.println(row);
                 System.out.println(String.format(" INSERT INTO meisoodev.user_white_list(white_list_id, created_by, created_dt, deleted, deleted_by, deleted_dt, plate_no, remark, plate_no_colour, phone, name, white_list_type) " +
-                        "VALUES(null, 1, '2025-08-01 00:00:00', 0, 1, '2035-08-01 00:00:00', %s, '≤–º≤≥µÕ≥“ª5¬º»Î20250808', %s, '19163154356', '∫´æ≤', 2);"
+                        "VALUES(null, 1, '2025-09-01 00:00:00', 0, 1, '2035-09-01 00:00:00', %s, '≤–º≤≥µÕ≥“ª5¬º»Î20250908', %s, '19163154356', '∫´æ≤', 2);"
                         ,row,row.length()==10?4:0));
 
             }
         });
 
-        URLEncoder.encode("");
-//        String ss=URLDecoder.decode("https://open.work.weixin.qq.com/wwopen/uriconfirm?err_type=param_err&uri=https%3A%2F%2Fopen.weixin.qq.com%2Fconnect%2Foauth2%2Fauthorize%3Fappid%3Dwx8697eec492141ca6%26redirect_uri%3Dhttp%253A%252F%252Ftxtestwww.meitianiot.com%252FroadParkScanPay%252Findex1.html%2523%252Fauthorization%253FscanType%253D1%2526plateNo%253D%2525E5%252590%252589LC0014%2526plateNoColour%253Dnull%2526parkId%253D1949727814434553856%2526howLong%253D137587000%2526parkItemName%253D2025001%2526isParkLock%253D0%2526payWay%253D30%2526inputMoney%253D2%2526payAbleMoney%253D2%2526orderIds%253D2508060947512197095%2526terminalLoginUserId%253D1952899886433045088%26response_type%3Dcode%26scope%3Dsnsapi_base%26state%3DSTATE%26connect_redirect%3D1%23wechat_redirect&ts=1754902479&nonce=1232afc65fcb2448&sig=bb1ce7c45a3d517f637391d1fb4a0e79c06f1029&sig_ver=2&bar_style_type=2&retry_time=1754902480041");
-        String ss=URLEncoder.encode("https://open.work.weixin.qq.com/wwopen/uriconfirm?err_type=param_err&uri=https%3A%2F%2Fopen.weixin.qq.com%2Fconnect%2Foauth2%2Fauthorize%3Fappid%3Dwx8697eec492141ca6%26redirect_uri%3Dhttp%253A%252F%252Ftxtestwww.meitianiot.com%252FroadParkScanPay%252Findex1.html%2523%252Fauthorization%253FscanType%253D1%2526plateNo%253D%2525E5%252590%252589LC0014%2526plateNoColour%253Dnull%2526parkId%253D1949727814434553856%2526howLong%253D137587000%2526parkItemName%253D2025001%2526isParkLock%253D0%2526payWay%253D30%2526inputMoney%253D2%2526payAbleMoney%253D2%2526orderIds%253D2508060947512197095%2526terminalLoginUserId%253D1952899886433045088%26response_type%3Dcode%26scope%3Dsnsapi_base%26state%3DSTATE%26connect_redirect%3D1%23wechat_redirect&ts=1754902479&nonce=1232afc65fcb2448&sig=bb1ce7c45a3d517f637391d1fb4a0e79c06f1029&sig_ver=2&bar_style_type=2&retry_time=1754902480041");
-        System.out.println(ss);
+//        URLEncoder.encode("");
+////        String ss=URLDecoder.decode("https://open.work.weixin.qq.com/wwopen/uriconfirm?err_type=param_err&uri=https%3A%2F%2Fopen.weixin.qq.com%2Fconnect%2Foauth2%2Fauthorize%3Fappid%3Dwx8697eec492141ca6%26redirect_uri%3Dhttp%253A%252F%252Ftxtestwww.meitianiot.com%252FroadParkScanPay%252Findex1.html%2523%252Fauthorization%253FscanType%253D1%2526plateNo%253D%2525E5%252590%252589LC0014%2526plateNoColour%253Dnull%2526parkId%253D1949727814434553856%2526howLong%253D137587000%2526parkItemName%253D2025001%2526isParkLock%253D0%2526payWay%253D30%2526inputMoney%253D2%2526payAbleMoney%253D2%2526orderIds%253D2508060947512197095%2526terminalLoginUserId%253D1952899886433045088%26response_type%3Dcode%26scope%3Dsnsapi_base%26state%3DSTATE%26connect_redirect%3D1%23wechat_redirect&ts=1754902479&nonce=1232afc65fcb2448&sig=bb1ce7c45a3d517f637391d1fb4a0e79c06f1029&sig_ver=2&bar_style_type=2&retry_time=1754902480041");
+//        String ss=URLEncoder.encode("https://open.work.weixin.qq.com/wwopen/uriconfirm?err_type=param_err&uri=https%3A%2F%2Fopen.weixin.qq.com%2Fconnect%2Foauth2%2Fauthorize%3Fappid%3Dwx8697eec492141ca6%26redirect_uri%3Dhttp%253A%252F%252Ftxtestwww.meitianiot.com%252FroadParkScanPay%252Findex1.html%2523%252Fauthorization%253FscanType%253D1%2526plateNo%253D%2525E5%252590%252589LC0014%2526plateNoColour%253Dnull%2526parkId%253D1949727814434553856%2526howLong%253D137587000%2526parkItemName%253D2025001%2526isParkLock%253D0%2526payWay%253D30%2526inputMoney%253D2%2526payAbleMoney%253D2%2526orderIds%253D2508060947512197095%2526terminalLoginUserId%253D1952899886433045088%26response_type%3Dcode%26scope%3Dsnsapi_base%26state%3DSTATE%26connect_redirect%3D1%23wechat_redirect&ts=1754902479&nonce=1232afc65fcb2448&sig=bb1ce7c45a3d517f637391d1fb4a0e79c06f1029&sig_ver=2&bar_style_type=2&retry_time=1754902480041");
+//        System.out.println(ss);
 //        System.out.println(rowStr.get());
 
 
