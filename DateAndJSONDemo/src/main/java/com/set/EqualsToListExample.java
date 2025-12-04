b4ff5227e7dc27c557e937f1fe29d2cb49eb587a
hanf666666 <hanjing_xl@163.com>
2025-12-04 17:35:09 +0800

    强制】关于 hashCode 和 equals 的处理，遵循如下规则：

---
+++ DateAndJSONDemo/src/main/java/com/set/EqualsToListExample.java
@@ -0,0 +1,76 @@
+package com.set;
+
+
+import java.util.HashSet;
+import java.util.Objects;
+import java.util.Set;
+/**
+ * 1. 【强制】关于 hashCode 和 equals 的处理，遵循如下规则：
+ * @author Hj
+ * @date 2025/12/2
+ */
+public class EqualsToListExample {
+    public static void main(String[] args) {
+        // 对象 1
+        Persion p1 = new Persion();
+        p1.setName("Java");
+        p1.setAge(18);
+        // 对象 2
+        Persion p2 = new Persion();
+        p2.setName("Java");
+        p2.setAge(18);
+        // 创建 Set 对象
+        Set<Persion> set = new HashSet<Persion>();
+        set.add(p1);
+        set.add(p2);
+        // 打印 Set 中的所有数据
+        set.forEach(p -> {
+            System.out.println(p);
+        });
+    }
+}
+
+
+class Persion {
+    private String name;
+    private int age;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true; // 引用相等返回 true
+        // 如果等于 null，或者对象类型不同返回 false
+        if (o == null || getClass() != o.getClass()) return false;
+        // 强转为自定义 Persion 类型
+        Persion persion = (Persion) o;
+        // 如果 age 和 name 都相等，就返回 true
+        return age == persion.age &&
+                Objects.equals(name, persion.name);
+    }
+
+    @Override
+    public int hashCode() {
+        // 对比 name 和 age 是否相等
+        return Objects.hash(name, age);
+    }
+
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public int getAge() {
+        return age;
+    }
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "Persion{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                '}';
+    }
+}
