c36ee34e7c4cdb29791b5302fe813084b9934b47
hanf666666 <hanjing_xl@163.com>
2025-12-23 10:05:41 +0800

    排查等于3的日志

--- Connectiondemo/src/main/java/com/date/splicingSqlmain2.java
+++ Connectiondemo/src/main/java/com/date/splicingSqlmain2.java
@@ -51,6 +51,19 @@ public class splicingSqlmain2 {
                 " ) t\n" +
                 "ORDER BY t.status ASC,t.validityEndDate ASC";
 
+
+       sql="select   order_item_no as orderItemNo, pay_app_id as payappId, app_id as payappAppId, pay_business_id as payBusinessId, pay_account_id as payAccountId, account_code as accountCode, server_account_code as serverAccountCode, pay_route_id as payRouteId,\n" +
+               "temp.settlement_flag as settlementFlag,temp.settlement_channel_id as settlementChannelId,temp.pay_acno as settlementChannelNo,rp2.attr_val as attrVal,\n" +
+               "rp2.settlement_flag as couponSettlementFlag\n" +
+               "from (\n" +
+               "\tselect r.*,rp.settlement_flag,rp.settlement_channel_id,st.pay_acno\n" +
+               "\tfrom pay_param_order_record r\n" +
+               "\tleft join  pay_param_order_record_attr rp\n" +
+               "\ton r.id=rp.pay_record_id and (rp.attr_type=1 or rp.attr_type is null) \n" +
+               "\tleft join settle_accounts_config st on rp.settlement_channel_id=st.id\n" +
+               "\n" +
+               "\twhere `order_item_no`=:orderItemNo and  r.deleted=0 \n" +
+               ") as temp left join pay_param_order_record_attr rp2 on  temp.id=rp2.pay_record_id and rp2.attr_type=2 limit 1";
         Map<String, Object> paramMap = new HashMap<>();
 
 //        if (Objects.nonNull(dto.getCityId())){
