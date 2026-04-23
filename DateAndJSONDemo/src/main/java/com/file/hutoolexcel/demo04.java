00e4d2f90846fabefa9630ddeda24f274f1459fe
hanf666666 <hanjing_xl@163.com>
2026-04-23 10:26:04 +0800

    excell

--- DateAndJSONDemo/src/main/java/com/file/hutoolexcel/demo04.java
+++ DateAndJSONDemo/src/main/java/com/file/hutoolexcel/demo04.java
@@ -21,7 +21,7 @@ public class demo04 {
         List<Map<String, Object>> oldMap1 = ExcelUtil.getReader(fileName, 0).readAll();
         //新数据
         List<Map<String, Object>> newMap2 = ExcelUtil.getReader(fileName, 1).readAll();
-        HashMap<String, String> stringStringHashMap = new HashMap<>();
+        HashMap<String, String> newRowHashMap = new HashMap<>();
         newMap2.forEach(newRowMap -> {
 //            System.out.println(map.get("车牌号").toString());
 //            System.out.println(map.get("车牌颜色").toString());
@@ -31,12 +31,12 @@ public class demo04 {
             if(newRowMap.get("车牌号")==null){
                 return;
             }
-//            String s = newRowMap.get("车牌号").toString().replaceAll("-", "") + newRowMap.get("车牌颜色").toString();
-            String s = newRowMap.get("车牌号").toString().replaceAll("-", "") ;
+            String s = newRowMap.get("车牌号").toString().replaceAll("-", "") + newRowMap.get("车牌颜色").toString();
+//            String s = newRowMap.get("车牌号").toString().replaceAll("-", "") ;
 
 //            System.out.println( s);
 //            stringStringHashMap.put(s,map.get("欠费金额(元)").toString()+map.get("欠费金额(元)").toString());
-            stringStringHashMap.put(s,newRowMap.get("车牌号").toString() + "\t" + newRowMap.get("车牌颜色").toString() + "\t" + newRowMap.get("欠费金额(元)").toString());
+            newRowHashMap.put(s,newRowMap.get("车牌号").toString() + "\t" + newRowMap.get("车牌颜色").toString() + "\t" + newRowMap.get("欠费金额(元)").toString());
         });
         oldMap1.forEach(oldRowMap -> {
 //            System.out.println(map);
@@ -45,10 +45,10 @@ public class demo04 {
 //            System.out.println(oldRowMap.get("车主电话").toString());
 //            System.out.println(oldRowMap.get("欠费金额(元)").toString());
 //            String s = map.get("车牌号").toString().replaceAll("-", "") + map.get("车牌颜色").toString();
-            String s = oldRowMap.get("车牌号").toString().replaceAll("-", "") ;
-//            String s = oldRowMap.get("车牌号").toString().replaceAll("-", "") + oldRowMap.get("车牌颜色").toString();
+//            String s = oldRowMap.get("车牌号").toString().replaceAll("-", "") ;
+            String s = oldRowMap.get("车牌号").toString().replaceAll("-", "") + oldRowMap.get("车牌颜色").toString();
 
-            System.out.println( oldRowMap.get("车牌号").toString() + "\t" + oldRowMap.get("车主电话").toString() + "\t" + oldRowMap.get("欠费金额(元)").toString() + "\t" + stringStringHashMap.get( s));
+            System.out.println( oldRowMap.get("车牌号").toString() + "\t" + oldRowMap.get("车主电话").toString() + "\t" + oldRowMap.get("欠费金额(元)").toString() + "\t" + newRowHashMap.get( s));
 //            System.out.println( oldRowMap.get("车牌号").toString() + "\t" + oldRowMap.get("车主电话").toString() + "\t" + stringStringHashMap.get( s));
 
         });
