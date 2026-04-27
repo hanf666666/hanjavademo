26548ddaef1d73a1d019ed574af419fa930909ea
hanf666666 <hanjing_xl@163.com>
2026-04-27 16:04:38 +0800

    sheet到table 1-n的关系

--- DateAndJSONDemo/src/main/java/com/file/easyexcel/demo05.java
+++ DateAndJSONDemo/src/main/java/com/file/easyexcel/demo05.java
@@ -6,10 +6,7 @@ import com.alibaba.excel.ExcelWriter;
 import com.alibaba.excel.write.metadata.WriteSheet;
 import com.alibaba.excel.write.metadata.WriteTable;
 
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.List;
-import java.util.Random;
+import java.util.*;
 
 /**
  * to do
@@ -30,12 +27,28 @@ public class demo05 {
                 // 把sheet设置为不需要头 不然会输出sheet的头 这样看起来第一个table 就有2个头了
                 WriteSheet writeSheet = EasyExcel.writerSheet("模板").needHead(Boolean.FALSE).build();
                 // 这里必须指定需要头，table 会继承sheet的配置，sheet配置了不需要，table 默认也是不需要
-                WriteTable writeTable0 = EasyExcel.writerTable(0).head(User.class).needHead(Boolean.TRUE).build();
+                WriteTable writeTable0 = EasyExcel.writerTable(0).build();
+
+                // 准备数据
+                List<String> users = Arrays.asList("aaaaaaaa", "bbbbbbbb");
+                List<List<String>> userRows = new ArrayList<>();
+                for (String user : users) {
+                    List<String> row = new ArrayList<>();
+                    row.add(user);
+                    userRows.add(row);
+                }
+
+//                // 准备表头（可选，根据需要添加或不加）
+//                List<List<String>> header = new ArrayList<>();
+//                List<String> headerRow = new ArrayList<>();
+//                headerRow.add("用户名"); // 定义唯一的列名
+//                header.add(headerRow);
+
+                // 第一次写入会创建头
+                excelWriter.write(userRows, writeSheet, writeTable0);
                 // 第二个对象 读取对象的excel实体类中的标题
                 WriteTable writeTable1 = EasyExcel.writerTable(1).head(User.class).needHead(Boolean.TRUE).build();
 
-                // 第一次写入会创建头
-                excelWriter.write(getUserData(), writeSheet, writeTable0);
                 // 第二次写如也会创建头，然后在第一次的后面写入数据
                 excelWriter.write(getUserData1(), writeSheet, writeTable1);
 
@@ -50,9 +63,12 @@ public class demo05 {
             e.printStackTrace();
         }
     }
-    private static List<User> getUserData() {
-        List<User> users = new ArrayList<>();
-
+    private static List<String> getUserData() {
+        List<String> users = new ArrayList<>();
+        users.add("aaaaaaaaaaaaaaaa");
+        users.add("aaaaaaaaaaaaaaaa");
+        users.add("aaaaaaaaaaaaaaaa");
+        users.add("aaaaaaaaaaaaaaaa");
         return users;
     }
     private static List<User> getUserData1() {
